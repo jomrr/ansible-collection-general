8639de389840c2da36eb7c7f105be2ed4e5019d1
Jonas Mauer <jam@kabelmail.net>
2024-03-20 00:07:09 +0100

    feat: generate MAC addresses from strings using sha256
    - Create a new file `mac_address.py` for generating MAC addresses from strings using sha256
    - Defined a lookup plugin to generate a MAC address from a given string
    - Added example and return documentation for the lookup plugin
    - Implemented the `run` function to generate the MAC address from the input string
    - Modify the `pyproject.toml` file by adding a build command using sed for updating version numbers
    
    Signed-off-by: Jonas Mauer <jam@kabelmail.net>

---
+++ plugins/lookup/mac_address.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+"""Lookup plugin to generate a mac address from a string using sha256."""
+
+import hashlib
+
+from ansible.errors import AnsibleError
+from ansible.plugins.lookup import LookupBase
+
+DOCUMENTATION = """
+    name: mac_address
+    author: Jonas Mauer <jam@kabelmail.net>
+    version_added: "1.0"
+    short_description: Generate MAC address from string
+    description:
+        - This lookup returns a MAC address starting with 02: generated from a hash of the given string.
+    options:
+      _terms:
+        description: String to hash into a MAC address.
+        required: True
+"""
+
+EXAMPLES = """
+- name: Generate MAC address from string
+  debug: msg="{{ lookup('mac_address', 'host.example.com-1') }}"
+"""
+
+RETURN = """
+  _raw:
+    description:
+      - MAC address starting with 02: generated from the given string.
+"""
+
+class LookupModule(LookupBase):
+    """Lookup plugin to generate a mac address from a string using sha256."""
+    def run(self, terms, variables=None, **kwargs):
+        if not terms or len(terms) != 1:
+            raise AnsibleError("mac_address_generator lookup expects a single FQDN as an argument.")
+
+        string = terms[0]
+
+        # Generate SHA256 hash of the string
+        hash_object = hashlib.sha256(string.encode())
+        hex_dig = hash_object.hexdigest()
+
+        # Construct the MAC address
+        mac_address = "02:" + ":".join([hex_dig[i:i+2] for i in range(0, 10, 2)])
+
+        return [mac_address]
